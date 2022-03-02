c597f31b833ca9c5dd43256202200b33c8bba5af
alfdaniel <daniel.alexandre22@hotmail.com>
2022-03-01 23:11:03 -0300

    Comentários das endpoints

--- cadastroprodutos/src/main/java/com/dev42/cadastroprodutos/Controller/ProductController.java
+++ cadastroprodutos/src/main/java/com/dev42/cadastroprodutos/Controller/ProductController.java
@@ -3,10 +3,19 @@ package com.dev42.cadastroprodutos.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dev42.cadastroprodutos.entities.Product;
 import com.dev42.cadastroprodutos.repository.ProductRepository;
 
 @RestController
@@ -16,11 +25,55 @@ public class ProductController {
 	@Autowired
 	private ProductRepository repository;
 	
-	
-	@GetMapping
-	public List findAll(){
-		return repository.findAll();
+	//http://localhost:8083/product
+	@PostMapping
+	public Product create(@RequestBody Product product){
+		return repository.save(product);
 	}
 	
-
+	//http://localhost:8083/product/id
+	@PutMapping(value = "{id}")
+	public ResponseEntity update(@PathVariable long id, @RequestBody Product product){
+		return repository.findById(id)
+				.map(record -> {
+					record.setCategory(product.getCategory());
+					record.setDescription(product.getDescription());
+					record.setFinalPrice(product.getFinalPrice());
+					record.setFinalPrice(product.getFinalPrice());
+					record.setFractionPrice(product.getFractionPrice());
+					record.setName(product.getName());
+					record.setImgUrl(product.getImgUrl());
+					record.setQuantity(product.getQuantity());
+					Product prod = repository.save(record);
+					return ResponseEntity.ok().body(prod);
+				}).orElse(ResponseEntity.notFound().build());
+	}
+	
+	
+	//Métodos Utilizando native query
+	
+	//http://localhost:8083/product/listquery
+	@GetMapping({"/listquery"})
+	public List findAllQuery(){
+		return repository.findAllNativeQuery();
+	}
+	
+	//http://localhost:8083/product/id
+	@GetMapping(value = "{id}")
+	public List findAllByIdQuery(@PathVariable long id){
+		return repository.findByIdNativeQuery(id);
+	}
+	
+	//http://localhost:8083/product/id
+	@DeleteMapping(value = {"id"})
+	public void deleteByIdQuery(@PathVariable long id){
+		repository.deleteByIdNativeQuery(id);
+	}
+	
+	//http://localhost:8083/product/name?name=name *esse ultimo name é o parâmetro
+	@GetMapping(value = "/name")
+	public List<Product> findAllByNameQuery(@RequestParam("name")String name){
+		return repository.findAllByNameNativeQuery(name);
+	}
+		
 }
--- cadastroprodutos/src/main/java/com/dev42/cadastroprodutos/repository/ProductRepository.java
+++ cadastroprodutos/src/main/java/com/dev42/cadastroprodutos/repository/ProductRepository.java
@@ -1,6 +1,11 @@
 package com.dev42.cadastroprodutos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.dev42.cadastroprodutos.entities.Product;
@@ -8,6 +13,18 @@ import com.dev42.cadastroprodutos.entities.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	@Query(value = "select * from product", nativeQuery = true)
+	List<Product> findAllNativeQuery();
+	
+	@Query(value = "select * from product i where i.id= ?", nativeQuery = true)
+	List<Product> findByIdNativeQuery(long id);
+	
+	@Modifying
+	@Query(value = "delete from product i where i.id= ?", nativeQuery= true )
+	void deleteByIdNativeQuery(@Param("id") long id);
+	
+	@Query(value = "SELECT * FROM product e WHERE e.name like %:name%", nativeQuery = true)
+	List<Product> findAllByNameNativeQuery(@Param("name") String name);
 	
 	
 }
